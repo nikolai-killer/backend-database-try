b179650d352c13182a008a216897aa68cad713ab
nikolai-killer <nikolai.killer@gmx.de>
2020-06-17 17:12:17 +0200

    cchanged

--- build/index.js
+++ build/index.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.greet = void 0;
-var address_entity_1 = require("./entities/address/address.entity");
-module.exports = address_entity_1.AddressEntity;
 exports.greet = function () {
     return 'Hello fremder';
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { AddressEntity } from './entities/address/address.entity';
 
 
-module.exports =  AddressEntity;
+
 
 export const greet = () => {
   return 'Hello fremder';
