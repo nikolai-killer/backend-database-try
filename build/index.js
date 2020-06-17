259bc0ca37d691af65afe65b6d04fbc6fdf5d121
nikolai-killer <nikolai.killer@gmx.de>
2020-06-17 15:44:06 +0200

    cchanged

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AddressEntity = void 0;
+var AddressEntity = require('./address/address.entity');
+exports.AddressEntity = AddressEntity;
+module.exports = AddressEntity;
+console.log(module.exports);
+//# sourceMappingURL=index.js.map
